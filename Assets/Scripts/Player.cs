414ae3f56bd807ca7edd49801259a99bc44e859e
S&K <sdg4631@gmail.com>
2018-05-08 15:02:36 +0900

    Flip Character Sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,12 +16,22 @@ public class Player : MonoBehaviour
 	void Update() 
 	{
 		Run();
+		FlipSprite();
 	}
 
-	void Run()
+	private void Run()
 	{
 		float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 and +1
 		Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);	
 		myRigidBody.velocity = playerVelocity;	
+	}
+
+	private void FlipSprite()
+	{
+		bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+		if (playerHasHorizontalSpeed)
+		{
+			transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+		}
 	}
 }
