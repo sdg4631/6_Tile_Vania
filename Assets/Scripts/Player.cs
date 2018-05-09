ae64c591339ac3996d127739992923f9ebfbbd35
S&K <sdg4631@gmail.com>
2018-05-09 12:17:57 +0900

    Climb Ladder

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,9 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour 
 {
 	// Config
-	[SerializeField] float runSpeed = 5f;
-	[SerializeField] float jumpSpeed = 5f;
+	[SerializeField] float runSpeed = 7f;
+	[SerializeField] float jumpSpeed = 17f;
+	[SerializeField] float climbSpeed = 7f;
 
 
 	// State
@@ -31,12 +32,13 @@ public class Player : MonoBehaviour
 		Run();
 		Jump();
 		FlipSprite();
+		ClimbLadder();
 	}
 
 	private void Run()
 	{
-		float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 and +1
-		Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);	
+		float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 and +1
+		Vector2 playerVelocity = new Vector2(horizontalThrow * runSpeed, myRigidBody.velocity.y);	
 		myRigidBody.velocity = playerVelocity;	
 
 		bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
@@ -61,5 +63,17 @@ public class Player : MonoBehaviour
 		{
 			transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
 		}
+	}
+
+	private void ClimbLadder()
+	{
+		if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Ladders"))) { return; }
+
+		float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+		Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+		myRigidBody.velocity = climbVelocity;
+
+		bool playerHasClimbingSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+		myAnimator.SetBool("Climbing", playerHasClimbingSpeed);		
 	}
 }
