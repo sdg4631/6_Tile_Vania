81d1870b331d2dc7f8eb1fee3072ca1ec2e88903
S&K <sdg4631@gmail.com>
2018-05-15 11:20:29 +0900

    Adding Coins and Persistent SFX

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour 
+{
+	[SerializeField] AudioClip coinPickupSFX;
+	
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+
+		// add coin to player score in GameSession
+
+		Destroy(gameObject);
+	}
+
+}
